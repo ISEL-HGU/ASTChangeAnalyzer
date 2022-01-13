8dcdd9cef7b96f4edf64689c82c84956c9d561c7
22000217 <80326384+22000217@users.noreply.github.com>
2022-01-13 17:09:21 +0900

    Finished basic settings

--- app/src/main/java/ASTChangeAnalyzer/Main.java
+++ app/src/main/java/ASTChangeAnalyzer/Main.java
@@ -3,12 +3,10 @@
  */
 package ASTChangeAnalyzer;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+public class Main {
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        
     }
 }
