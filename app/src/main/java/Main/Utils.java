7353f8fc1e845d5c4d1d116a8e01fa3fe60eff8f
22000217 <80326384+22000217@users.noreply.github.com>
2022-01-18 14:25:47 +0900

    [FEAT] added gumtreediff and related code

--- app/src/main/java/Main/Utils.java
+++ app/src/main/java/Main/Utils.java
@@ -7,8 +7,13 @@ import org.eclipse.jgit.diff.DiffAlgorithm;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.diff.RawTextComparator;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
 import org.eclipse.jgit.util.io.DisabledOutputStream;
 
 public class Utils {
@@ -32,5 +37,49 @@ public class Utils {
 		}
 
 		return diffs;
+	}
+	
+	public static String getKeyName(String commitName, String newPath) {
+
+		if (newPath.contains("\\")) {
+			newPath = newPath.replace("\\", "-");
+		}
+		if (newPath.contains("/")) {
+			newPath = newPath.replace("/", "-");
+		}
+
+		return commitName + "-" + newPath;
+	}
+	
+	public static String fetchBlob(Repository repo, String revSpec, String path) {
+
+		try {
+			// Resolve the revision specification
+			final ObjectId id = repo.resolve(revSpec);
+
+			// Makes it simpler to release the allocated resources in one go
+			ObjectReader reader = repo.newObjectReader();
+
+			// Get the commit object for that revision
+			RevWalk walk = new RevWalk(reader);
+			RevCommit commit = walk.parseCommit(id);
+			walk.close();
+
+			// Get the revision's file tree
+			RevTree tree = commit.getTree();
+			// .. and narrow it down to the single file's path
+			TreeWalk treewalk = TreeWalk.forPath(reader, path, tree);
+			if (treewalk != null) {
+				// use the blob id to read the file's data
+				byte[] data = reader.open(treewalk.getObjectId(0)).getBytes();
+				reader.close();
+				return new String(data, "utf-8");
+			} else {
+				return "";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 }
