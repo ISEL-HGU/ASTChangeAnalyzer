290947d654c92adb6f2b975c66a80c4ab307cf87
zackcglee <leechanggong@gmail.com>
2022-02-14 11:24:08 +0900

    graph.py corrected

--- app/src/main/java/edu/handong/csee/isel/ChangeAnalysis/graph.py
+++ app/src/main/java/edu/handong/csee/isel/ChangeAnalysis/graph.py
@@ -9,16 +9,15 @@ with open(../../../../Statistic.txt) as f:
   lines = f.readlines()
 
 for line in lines :
-    if line.find("Analyzed Change size : ") != -1 :
-        pos = line.rindex("Change size : ")
-        x.append(int(line[pos+14:]))
+    if line.find("L Analyzed Change size: ") != -1 :
+        pos = line.rindex("L Analyzed Change size: ")
+        x.append(int(line[pos+24:]))
     if line.find("file level) size: ") != -1 :
         pos = line.rindex("file level) size: ")
         y1.append(int(line[pos+18:]))
     if line.find("core level) size: ") != -1 :
         pos = line.rindex("core level) size: ")
         y2.append(int(line[pos+18:]))
-
 
 print("file level index\n")
 print(y1)
