636ee6dafd048919d8b0051d92511e2a3b96c2fd
22000217 <80326384+22000217@users.noreply.github.com>
2022-02-17 15:13:16 +0900

    [FIX] Exception handling #17

--- app/src/main/java/edu/handong/csee/isel/RepoMiner/ChangeMiner.java
+++ app/src/main/java/edu/handong/csee/isel/RepoMiner/ChangeMiner.java
@@ -54,27 +54,32 @@ public class ChangeMiner {
 				RevCommit parent = commit.getParent(0);
 				List<DiffEntry> diffs = RepoUtils.diff(parent, commit, repo);
 				for (DiffEntry diff : diffs) {
-					String oldPath = diff.getOldPath();
-					String newPath = diff.getNewPath();
-					if (newPath.indexOf("Test") >= 0 || !newPath.endsWith(fileExtension))
+					try {
+						String oldPath = diff.getOldPath();
+						String newPath = diff.getNewPath();
+						if (newPath.indexOf("Test") >= 0 || !newPath.endsWith(fileExtension))
+							continue;
+						String srcFileSource = RepoUtils.fetchBlob(repo, commit.getId().getName() + "~1", oldPath);
+						String dstFileSource = RepoUtils.fetchBlob(repo, commit.getId().getName(), newPath);
+						ChangeInfo changeInfo = new ChangeInfo(repo.getDirectory().getParent(), commit.name());
+						switch (DiffTool) {
+							case "LAS":
+								LASTool las = new LASTool(fileExtension, srcFileSource, dstFileSource);
+								changeInfo = las.constructChange(changeInfo);
+								break;
+							default:
+								GumTree gumtree = new GumTree(fileExtension, srcFileSource, dstFileSource);
+								changeInfo = gumtree.constructChange(changeInfo);
+								break;
+						}
+						changeAnalyzer.generateMap(changeInfo, language);
+						if (isAnalysis && changeAnalyzer.getTotalCount()==volumeCount) {
+							changeAnalyzer.printStatistic();
+							volumeCount += volumePortion;
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
 						continue;
-					String srcFileSource = RepoUtils.fetchBlob(repo, commit.getId().getName() + "~1", oldPath);
-					String dstFileSource = RepoUtils.fetchBlob(repo, commit.getId().getName(), newPath);
-					ChangeInfo changeInfo = new ChangeInfo(repo.getDirectory().getParent(), commit.name());
-					switch (DiffTool) {
-						case "LAS":
-							LASTool las = new LASTool(fileExtension, srcFileSource, dstFileSource);
-							changeInfo = las.constructChange(changeInfo);
-							break;
-						default:
-							GumTree gumtree = new GumTree(fileExtension, srcFileSource, dstFileSource);
-							changeInfo = gumtree.constructChange(changeInfo);
-							break;
-					}
-					changeAnalyzer.generateMap(changeInfo, language);
-					if (isAnalysis && changeAnalyzer.getTotalCount()==volumeCount) {
-						changeAnalyzer.printStatistic();
-						volumeCount += volumePortion;
 					}
 				}
 			}
