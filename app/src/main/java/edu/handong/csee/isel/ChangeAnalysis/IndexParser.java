5d9eecd55c4885f597ce1d10ba5e6b84660aa850
leechanggong <leechanggong@leechanggongui-MacBookPro.local>
2022-03-06 22:49:32 +0900

    [REFACTOR]Corrected IndexParser #27

--- app/src/main/java/edu/handong/csee/isel/ChangeAnalysis/IndexParser.java
+++ app/src/main/java/edu/handong/csee/isel/ChangeAnalysis/IndexParser.java
@@ -23,7 +23,10 @@ public class IndexParser {
             PrintWriter out = new PrintWriter(fos);
 
             for(String key : hashMap.keySet()) {
-                out.print(key + "-" + hashMap.get(key) + "\n");
+                out.print(key);
+                for(String contents : hashMap.get(key))
+                    out.print(","+hashMap.get(key));
+                out.print("\n");
             }
             out.flush();
             out.close();
@@ -44,66 +47,63 @@ public class IndexParser {
     }
 
     public void appendIndex(File file) {
-//        String thisLine = "";
-//
-//        try {
-//            File outFile = null;
-//            FileInputStream fis;
-//
-//            int fileCounter = 0;
-//            boolean found = false, found2 = false;
-//
-//            for(String fKey : this.hashMap.keySet()) {
-//                for(String hKey : this.hashMap.get(fKey).keySet()) {
-//                    String hashCode = fKey + "-" + hKey;
-//                    for(String content : this.hashMap.get(fKey).get(hKey)) {
-//                        if(fileCounter == 0)
-//                            fis = new FileInputStream(file);
-//                        else
-//                            fis = new FileInputStream(outFile);
-//                        BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-//                        outFile = new File(path+"/$" +Integer.toString(fileCounter)+ ".tmp");
-//                        FileOutputStream fos = new FileOutputStream(outFile);
-//                        PrintWriter out = new PrintWriter(fos);
-//
-//                        while ((thisLine = in.readLine()) != null) {
-//                            String [] row = thisLine.split(",");
-//                            if (row[0].equals(hashCode)) {
-//                                thisLine = thisLine + content + ",";
-//                                found = true;
-//                            }
-//                            out.println(thisLine);
-//                        }
-//                        if(!found)
-//                            out.println(hashCode + "," + content + ",");
-//                        found = false;
-//                        out.flush();
-//                        out.close();
-//                        in.close();
-//                        fis = new FileInputStream(outFile);
-//                        fileCounter++;
-//                    }
-//                }
-//            }
-//
-//            try {
-//                if(outFile == null)
-//                    outFile = file;
-//                file.delete();
-//                outFile.renameTo(file);
-//                for(int z = 0; z < fileCounter-1; z++) {
-//                    new File(path + "/$" + Integer.toString(z) + ".tmp").delete();
-//                }
-//            } catch (NullPointerException e) {
-//                System.out.println("There is a problem with a repo. Skipping indexing");
-//                return;
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        if(file.exists())
-//            sortIndex(file);
+        String thisLine = "";
+
+        try {
+            File outFile = null;
+            FileInputStream fis;
+
+            int fileCounter = 0;
+            boolean found = false, found2 = false;
+
+            for(String key : this.hashMap.keySet()) {
+                for(String contents : this.hashMap.get(key)) {
+                    if(fileCounter == 0)
+                        fis = new FileInputStream(file);
+                    else
+                        fis = new FileInputStream(outFile);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+                    outFile = new File(path+"/$" +Integer.toString(fileCounter)+ ".tmp");
+                    FileOutputStream fos = new FileOutputStream(outFile);
+                    PrintWriter out = new PrintWriter(fos);
+
+                    while ((thisLine = in.readLine()) != null) {
+                        String [] row = thisLine.split(",");
+                        if (row[0].equals(key)) {
+                            thisLine = thisLine + contents + ",";
+                            found = true;
+                        }
+                        out.println(thisLine);
+                    }
+                    if(!found)
+                        out.println(key + "," + contents + ",");
+                    found = false;
+                    out.flush();
+                    out.close();
+                    in.close();
+                    fis = new FileInputStream(outFile);
+                    fileCounter++;
+                }
+            }
+
+            try {
+                if(outFile == null)
+                    outFile = file;
+                file.delete();
+                outFile.renameTo(file);
+                for(int z = 0; z < fileCounter-1; z++) {
+                    new File(path + "/$" + Integer.toString(z) + ".tmp").delete();
+                }
+            } catch (NullPointerException e) {
+                System.out.println("There is a problem with a repo. Skipping indexing");
+                return;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(file.exists())
+            sortIndex(file);
     }
 
 
